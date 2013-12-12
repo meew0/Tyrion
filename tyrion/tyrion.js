a2a023a75e76f1080922161c76d3fac3a8eae9a6
meew0 <blactbt@live.de>
2013-12-12 17:59:37 +0100

    Added some functionality (doesn't really work yet)

--- tyrion/tyrion.js
+++ tyrion/tyrion.js
@@ -1,2 +1,19 @@
 $(document).ready(function() {
+	var w = $(window);
+	var text = "";
+	w.scroll(function() {
+		$('*[ld-for]').each(function() {
+			var lf = $(this).attr("ld-for");
+			var to = $(this).offset().top - w.scrollTop;
+			console.log(to);
+			$(lf).each(function() {
+				var offset = $(this).offset();
+				var top = offset.top + to;
+				if(top <= w.scrollTop()) {
+					text = $(this).attr('ld-info');
+				}
+			});
+			$(this).html(text);
+		});
+	});
 });
