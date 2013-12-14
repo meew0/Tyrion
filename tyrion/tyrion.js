11d47a1bfe30840accc029b8bfa981c80851c279
miras <blactbt@live.de>
2013-12-14 09:57:37 +0100

    ld-hover works properly

--- tyrion/tyrion.js
+++ tyrion/tyrion.js
@@ -1,8 +1,9 @@
 $(document).ready(function() {
+	$('.ld-hover, .ld-scroll').hide();
 	var w = $(window);
 	var text = "";
 	w.scroll(function() {
-		$('*[ld-for]').each(function() {
+		$('.ld-scroll').each(function() {
 			var lf = $(this).attr("ld-for");
 			var to = $(this).offset().top - w.scrollTop;
 			console.log(to);
@@ -14,6 +15,27 @@ $(document).ready(function() {
 				}
 			});
 			$(this).html(text);
+		});
+	});
+
+	$('.ld-hover').each(function() {
+		var to = $(this);
+		var lf = $(this).attr("ld-for");
+		var ofs = 0;
+		if(to.attr("ld-offset") != undefined) {
+			ofs = to.attr("ld-offset");
+		}
+		$(lf).mousemove(function(evt) {
+			to.children('p').html($(this).attr("ld-info"));
+			if(to.hasClass("ld-follow-mouse")) {
+				to.css('top', evt.pageY + parseInt(ofs));
+			} else {
+				to.css('top', $(this).offset().top + ofs);
+			}
+			to.show();
+		});
+		$(lf).mouseout(function(evt) {
+			to.hide();
 		});
 	});
 });
